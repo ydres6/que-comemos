582003d7a8a184194a10b2626b7ffacb6dfbd49b
ANDRES EMANUEL TASO <33707409@ort>
2019-10-30 19:29:06 -0300

    solicitar camara

--- src/components/Alimento.vue
+++ src/components/Alimento.vue
@@ -1,5 +1,17 @@
 <template>
     <div id="alimento">
     <h1>Alimento</h1>
+
+    <button class="btn btn-primary" @click="solicitarCamara()">Escanear Codigo</button>
     </div>
 </template>
+
+<script>
+export default {
+  methods:{
+      solicitarCamara(){
+
+      }
+  }
+}
+</script>
