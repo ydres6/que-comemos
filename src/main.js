ac261cbec20f5e3826de6708be9a17c7bb6b8dd6
ANDRES EMANUEL TASO <33707409@ort>
2019-10-30 21:03:32 -0300

    grafico

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import App from'./App.vue'
-//import Vuetify from 'vuetify/lib'
-//import 'vuetify/dist/vuetify.min.css'
+import "@/plugins/echarts"
+
+
 
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
-//Vue.use(Vuetify)
+
 Vue.use(BootstrapVue)
 Vue.use(VueRouter) 
 Vue.config.productionTip = false
 
+
  
 import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 import {routes} from './routes'
 const router = new VueRouter({
