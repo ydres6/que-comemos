cdf039bf1a5142578fb2906fc636dfbded0464c0
ANDRES EMANUEL TASO <33707409@ort>
2019-10-30 22:42:13 -0300

    qr

---
+++ src/components/camara/Qr.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="camara">
+
+</div>
+</template>
+<script>
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
+export default {
+    
+  components: {
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture
+  }
+}
+</script>
